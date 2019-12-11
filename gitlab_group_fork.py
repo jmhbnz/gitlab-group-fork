5061ea96852c5073f2e9ab5899685855161073e8
Brad Downey <bdowney@gitlab.com>
2019-12-11 13:58:56 -0300

    Add url and token parse

--- gitlab_group_fork.py
+++ gitlab_group_fork.py
@@ -1,9 +1,43 @@
 #!/usr/bin/env python3.7
 """GitLab Group Fork - Fork and entire group of projects in a tree to another namespace"""
 
+import sys
+import os
+import argparse
 import gitlab
 
-if __name__ != "__main__":
-    exit(1)
+def main():
+    """Main Function"""
+    options = parse_cli()
+    glab = gitlab.Gitlab(options.url, options.token)
 
-print("Hello World")
+def parse_cli():
+    """Parse CLI Options and return, fail on no valid token"""
+    parser = argparse.ArgumentParser("usage: %prog [options] src_group dest_group")
+
+    parser.add_argument("-u", "--url",
+                        dest="url",
+                        default="https://gitlab.com",
+                        help="base URL of the GitLab instance")
+
+    parser.add_argument("-t", "--token",
+                        dest="token",
+                        default='',
+                        help="API token")
+
+    options = parser.parse_args()
+
+    if options.token == "":
+        if os.getenv("GITLAB_TOKEN") is not None:
+            options.token = os.getenv("GITLAB_TOKEN")
+        else:
+            print("API Token Required - Not found in options or environment variables")
+            sys.exit(1)
+    return options
+
+
+
+if __name__ == "__main__":
+    main()
+
+print("End of Script")
