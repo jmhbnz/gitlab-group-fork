8d58068cd4d07bf8596d1563e2236cc0ff370842
Brad Downey <bdowney@gitlab.com>
2019-12-11 23:26:35 -0300

    Update formatting for pylint

--- gitlab_group_fork.py
+++ gitlab_group_fork.py
@@ -10,7 +10,7 @@ from treelib import Tree
 
 def main():
     """Main Function"""
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     options = parse_cli()
     glab = gitlab.Gitlab(options.url, options.token)
     read_src_group(glab, options.srcGroup)
@@ -48,29 +48,40 @@ def parse_cli():
             sys.exit(1)
     return options
 
-def read_src_group(gl, src):
+def read_src_group(glab, src):
     """Read source group tree from gitlab server"""
-    logging.info("Attemping to read source group '%s/%s'", gl.url, src)
+    logging.info("Attemping to read source group '%s/%s'", glab.url, src)
     src_group_tree = Tree()
-    top_level_group = gl.groups.get(src, include_subgroups=True)
-    src_group_tree.create_node(top_level_group.path, top_level_group.id, data=top_level_group.name)
+    top_level_group = glab.groups.get(src, include_subgroups=True)
+    src_group_tree.create_node(
+        top_level_group.path,
+        top_level_group.id,
+        data=top_level_group.name)
     def get_sub_groups(parent):
         logging.debug('Looking for sub-groups in %s', parent.full_path)
-        new_top = gl.groups.get(parent.id, include_subgroups=True)
+        new_top = glab.groups.get(parent.id, include_subgroups=True)
         subgroups = new_top.subgroups.list(all_available=True)
         for sub in subgroups:
             logging.debug('Found sub-group %s', sub.full_path)
-            src_group_tree.create_node(sub.path, sub.id, parent=new_top.id, data=sub.name)
+            src_group_tree.create_node(
+                sub.path,
+                sub.id,
+                parent=new_top.id,
+                data=sub.name)
             logging.debug('Added node to tree with name %s and id %s', sub.path, sub.id)
-            new_parent = gl.groups.get(sub.id, include_subgroups=True)
+            new_parent = glab.groups.get(sub.id, include_subgroups=True)
             new_subgroup = new_parent.subgroups.list(all_available=True)
             for child in new_subgroup:
                 logging.debug('Traversing group %s', child.full_path)
-                src_group_tree.create_node(child.path, child.id, parent=new_parent.id, data=child.name)
+                src_group_tree.create_node(
+                    child.path,
+                    child.id,
+                    parent=new_parent.id,
+                    data=child.name)
                 get_sub_groups(child)
     get_sub_groups(top_level_group)
+    logging.info('Found %s sub-groups', len(src_group_tree)-1)
     src_group_tree.show()
-    print(src_group_tree.to_json(with_data=True))
     return src_group_tree
 
 
